2d77bf5b8b778f6326e9eb0b77923b94b490b678
Olga Sonina <olga.g.sonina@gmail.com>
2023-05-13 15:13:12 +0200

    cleaned

--- dashboard_stream.py
+++ dashboard_stream.py
@@ -53,14 +53,6 @@ col1, col2 = st.columns(([20,7]))
 with col1:
     
     st.markdown('This is a tool to estimate clients capacity to repay a loan.')
-    # for percent_complete in range(100):
-    #     time.sleep(0.1)
-    #     progress_text = "Operation in progress. Please wait."
-    #     my_bar = st.progress(0, text=progress_text)
-
-    #     my_bar.progress(percent_complete + 1, text=progress_text)
-
-    
 
     
 
@@ -86,19 +78,6 @@ with col1:
     st.write("Show statistics: ")
 
 
-    #inputs = X.to_dict(orient="list")
-    #prediction = requests.post(url=DEPLOYED_MODEL,
-                            #json={"inputs": inputs},
-                           #headers=headers)
-
-    # Extract the response data as a dictionary
-    #response_data = prediction.json()
-
-
-    
-
-    
-
 
 
 
@@ -106,57 +85,6 @@ with col1:
     model_uri = "mlruns/0/66bb88985bd041819b6f3f3bb9cf2d50/artifacts/mlflow_model"
     model = mlflow.pyfunc.load_model(model_uri)
 
-
-   
-
-
-    from lightgbm import LGBMClassifier
-
-    
-    # params = {
-    # 'objective': 'binary',
-    # 'metric': 'f1_score',
-    # 'colsample_bytree': 0.4069597159452327,
-    #  'learning_rate': 0.057736760620294536,
-    #  'max_depth': 11, 
-    #  'min_child_samples': 11,
-    #  'n_estimators': 153,
-    #  'num_leaves': 99,
-    #  'reg_alpha': 9.822683433294355, 
-    #  'reg_lambda': 5.167358912710143,
-    #  'subsample': 0.3347462573473681,
-    # 'random_state': 42
-    # }
-    # clf = LGBMClassifier(**params)
-    # X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
-
-    # # fit the classifier to the training data
-    # clf.fit(X_train, y_train)
-
-
-    # st.set_option('deprecation.showPyplotGlobalUse', False)
-    # import shap
-    # shap_values = shap.TreeExplainer(clf).shap_values(X_test)
-    # fig=shap.summary_plot(shap_values[1], X_test)
-
-    # st.pyplot(fig)
-
-    # Print the response data
-    #st.write(response_data)
-        #if submit:
-
-
-    # progress_text = "Answered questions progress"
-    # my_bar = st.progress(0, text=progress_text)
-
-    # uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-    # for uploaded_file in uploaded_files:
-    #     bytes_data = uploaded_file.read()
-    #     st.write("filename:", uploaded_file.name)
-    #     st.write(bytes_data)
-    
-    # calc_contents = '''This is some text'''
-    # st.download_button('Calculate',calc_contents)
 
     
 with col2:
